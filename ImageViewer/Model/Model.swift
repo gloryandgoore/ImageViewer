0483104ea7ba062a5429beba8921461e63ee8099
Caseyann <goore_44@hotmail.ca>
2022-10-11 15:20:43 -0400

    first commit

---
+++ ImageViewer/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  ImageViewer
+//
+//  Created by Caseyann Goore on 2022-10-11.
+//
+
+import Foundation
---
+++ ImageViewer/Model/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  ImageViewer
+//
+//  Created by Caseyann Goore on 2022-10-11.
+//
+
+import Foundation
